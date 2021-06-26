b8987cfa95d9d0966df96de999d8b847f893af14
Akshay <akshayyadav04031998@gmail.com>
2021-06-26 18:33:59 +0530

    removeItemfom cart in cart slice

--- src/components/store/cart-slice.js
+++ src/components/store/cart-slice.js
@@ -19,7 +19,16 @@ const cartSlice = createSlice({
         existingItem.price = existingItem.price + newItem.price;
       }
     },
-    removeItemfromCart() {},
+    removeItemfromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
   },
 });
 export const cartActions = cartSlice.actions;
